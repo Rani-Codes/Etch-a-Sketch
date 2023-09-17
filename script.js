2f05074a9c8fa970acdfc61e65254dedd027ad5c
Rani-Codes <92886322+Rani-Codes@users.noreply.github.com>
2023-09-16 21:00:10 -0700

    button works but doesn't refresh after each click

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
 const gridContainer = document.querySelector('.gridContainer');
-const divArray = [];
+const button = document.querySelector('button');
+let gridSize;
 
-for (i = 0; i < 16; i++) {
-    const rowContainer = document.createElement('div');
-    rowContainer.classList.add('row');
-    gridContainer.append(rowContainer);
-    const div = document.createElement('div');
-    for(j = 0; j < 16; j++) {
-        const div = document.createElement('div');
-        console.log('This is a run')
-        div.classList.add('single');
-        rowContainer.append(div);
+button.addEventListener("click", e => {
+    gridSize = prompt("How large do you want the grid to be? Choose from 1 to 100.", 8)
+    if(gridSize < 1 || gridSize > 100) {
+        gridSize = prompt("How large do you want the grid to be? Choose from 1 to 100.", 8)
     }
-}
+    for (i = 0; i < gridSize; i++) {
+        const rowContainer = document.createElement('div');
+        rowContainer.classList.add('row');
+        gridContainer.append(rowContainer);
+        const div = document.createElement('div');
+        for(j = 0; j < gridSize; j++) {
+            const div = document.createElement('div');
+            div.classList.add('single');
+            rowContainer.append(div);
+        }
+    }
+})
+
+
+
+
+
 
 
